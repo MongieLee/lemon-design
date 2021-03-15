aa696e644fe188cd0ea8994ec4cb6b96e41bdaf6
MongieLee <le30n@outlook.com>
2021-03-15 23:59:23 +0800

    让滚动条支持鼠标拖动

--- lib/scroll/Scroll.tsx
+++ lib/scroll/Scroll.tsx
@@ -15,14 +15,27 @@ class Scroll extends React.Component<LdScrollProps, State> {
     barHeight: 0,
     barTop: 0,
   };
+  public dragging: boolean = false;
+  public firstY: number = 0;
+  public firstBarTop: number;
   public containerRef = React.createRef<HTMLDivElement>();
+
   public componentDidMount = () => {
+    window.addEventListener("mousemove", this.onMouseMove);
+    window.addEventListener("mouseup", this.onMouseUp);
+
     const { current } = this.containerRef;
     const scrollHeight = current!.scrollHeight;
     const viewHeight = current!.getBoundingClientRect().height;
     const barHeight = (viewHeight * viewHeight) / scrollHeight;
     this.setState({ barHeight });
   };
+
+  public componentWillUnmount = () => {
+    window.removeEventListener("mousemove", this.onMouseMove);
+    window.removeEventListener("mouseup", this.onMouseUp);
+  };
+
   public onScroll: UIEventHandler = () => {
     const { current } = this.containerRef;
     const scrollHeight = current!.scrollHeight;
@@ -30,6 +43,31 @@ class Scroll extends React.Component<LdScrollProps, State> {
     const scrollTop = current!.scrollTop;
     const barTop = (scrollTop * viewHeight) / scrollHeight;
     this.setState({ barTop });
+  };
+
+  public setBarTop = (scrollTop: number) => {
+    if (scrollTop < 0) return;
+    const { current } = this.containerRef;
+    const scrollHeight = current!.scrollHeight;
+    const viewHeight = current!.getBoundingClientRect().height;
+    const maxBarTop = ((scrollHeight - viewHeight) * viewHeight) / scrollHeight;
+    if (scrollTop > maxBarTop) return;
+    this.setState({ barTop: scrollTop });
+  };
+
+  public onMouseDown: React.MouseEventHandler = (e) => {
+    this.dragging = true;
+    this.firstY = e.clientY - this.firstY;
+    this.firstBarTop = this.state.barTop;
+  };
+  public onMouseMove = (event: MouseEvent) => {
+    if (this.dragging) {
+      const delta = event.clientY - this.firstY;
+      this.setBarTop(this.firstBarTop + delta);
+    }
+  };
+  public onMouseUp = () => {
+    this.dragging = false;
   };
 
   render() {
@@ -47,6 +85,7 @@ class Scroll extends React.Component<LdScrollProps, State> {
         </div>
         <div className="ld-scroll-track">
           <div
+            onMouseDown={this.onMouseDown}
             className="ld-scroll-bar"
             style={{ height: barHeight, transform: `translateY(${barTop}px)` }}
           ></div>
